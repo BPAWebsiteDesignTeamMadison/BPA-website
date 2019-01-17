412e9f63ccf012097a87f100f5bb6c6a61501fa5
Will Madsen <45468528+willmadsen2@users.noreply.github.com>
2019-01-16 21:33:53 -0700

    Add files via upload

---
+++ anchor_offset.js
@@ -0,0 +1,12 @@
+function offsetAnchor() {
+  if (location.hash.length !== 0) {
+    window.scrollTo(window.scrollX, window.scrollY - 70);
+  }
+}
+$(document).on('click', 'a[href^="#"]', function(event) {
+  window.setTimeout(function() {
+    offsetAnchor();
+  }, 0);
+});
+
+window.setTimeout(offsetAnchor, 0);    
