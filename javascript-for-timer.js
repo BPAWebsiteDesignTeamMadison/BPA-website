58a972a3e449162c666ea34a69284f2e012c436b
Will Madsen <45468528+willmadsen2@users.noreply.github.com>
2019-01-17 08:41:51 -0700

    Add files via upload

--- javascript-for-timer.js
+++ javascript-for-timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("may 1, 2019 0:0:0").getTime();
+var countDownDate = new Date("may 6, 2020 0:0:0").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
